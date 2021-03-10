9efc4f82b5b06340490ea90b936bfc6fc14f6a78
cereniyim <ceren@datarevenue.com>
2021-03-10 19:33:49 +0300

    initial folder structure and sketch of data loading
    update requirements with ijson
    
    --- don't change below this line ---
    MLOPS-131 #time 47m

---
+++ spec2vec-mlops/tasks/load_data.py
@@ -0,0 +1,6 @@
+class DataLoader:
+    def __init__(self):
+        pass
+
+    def load_gnps_json(self, path: str):
+        pass
---
+++ spec2vec-mlops/test/tasks/test_load_data.py
@@ -0,0 +1,5 @@
+import pytest
+
+
+def test_load_gnps_json():
+    pass
