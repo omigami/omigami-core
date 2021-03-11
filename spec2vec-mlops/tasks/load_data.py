013340b2e2732e381f671108538dcff32a8f20fa
cereniyim <ceren@datarevenue.com>
2021-03-11 15:16:39 +0300

    implement load_gnps_json and test it with small and big json
    add drfs to environment.frozen.yaml
    
    --- don't change below this line ---
    MLOPS-131 #time 2h 22m

--- spec2vec-mlops/tasks/load_data.py
+++ spec2vec-mlops/tasks/load_data.py
@@ -1,6 +1,16 @@
+from typing import Dict
+from urllib.request import urlopen
+
+import ijson
+
+
 class DataLoader:
     def __init__(self):
         pass
 
-    def load_gnps_json(self, path: str):
-        pass
+    def load_gnps_json(self, uri: str) -> Dict:
+        uri = urlopen(uri)
+        items = ijson.items(uri, "item")
+        objects = ({k: item[k] for k in item.keys()} for item in items)
+        for object in objects:
+            yield object
--- spec2vec-mlops/test/tasks/test_load_data.py
+++ spec2vec-mlops/test/tasks/test_load_data.py
@@ -1,5 +1,19 @@
 import pytest
 
+from tasks.load_data import DataLoader
 
-def test_load_gnps_json():
-    pass
+pytestmark = pytest.mark.skip
+
+
+@pytest.mark.parametrize(
+    "uri",
+    [
+        "https://raw.githubusercontent.com/MLOps-architecture/share/main/test_data/SMALL_GNPS.json",
+        "https://gnps-external.ucsd.edu/gnpslibrary/ALL_GNPS.json",
+    ],
+)
+def test_load_gnps_json_with_small_and_big_file(uri):
+    dl = DataLoader()
+    res = dl.load_gnps_json(uri)
+
+    assert res
