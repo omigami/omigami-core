5a0650d4d4d86a281ed361d773ff70e9c4fec313
cereniyim <ceren@datarevenue.com>
2021-03-12 14:00:52 +0300

    add logging for loading data
    --- don't change below this line ---
    MLOPS-131 #time 38m

--- spec2vec_mlops/tasks/load_data.py
+++ spec2vec_mlops/tasks/load_data.py
@@ -1,7 +1,11 @@
+import logging
 from typing import Dict
 from urllib.request import urlopen
 
 import ijson
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 class DataLoader:
@@ -10,6 +14,7 @@ class DataLoader:
 
     def load_gnps_json(self, uri: str) -> Dict:
         uri = urlopen(uri)
+        logger.info(f"Loading data from {uri}... This might take a while.")
         items = ijson.items(uri, "item", multiple_values=True)
         for item in items:
             yield item
