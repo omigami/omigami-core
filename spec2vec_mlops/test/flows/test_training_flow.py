b9cc885b5624878eae07ac828ceee48a625ea246
cereniyim <ceren@datarevenue.com>
2021-03-18 13:40:27 +0300

    remove test asset from test_training_flow.py and optimize imports in test_load_data.py after merge
    --- don't change below this line ---
    MLOPS-159 #time 23m

--- spec2vec_mlops/test/flows/test_training_flow.py
+++ spec2vec_mlops/test/flows/test_training_flow.py
@@ -1,14 +1,4 @@
-from pathlib import Path
-
-import pytest
-
 from spec2vec_mlops.flows.training_flow import spec2vec_train_pipeline_local
-
-
-@pytest.fixture()
-def gnps_small_json():
-    ASSET_DIR = str(Path(__file__).parents[1] / "assets" / "SMALL_GNPS.json")
-    return f"file://{ASSET_DIR}"
 
 
 def test_spec2vec_train_pipeline_local(gnps_small_json):
