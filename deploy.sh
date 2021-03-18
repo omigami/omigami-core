7b86972b7ea102c0d9d9a8e044b8eeb43f8e74a0
Thales <thales@datarevenue.com>
2021-03-18 14:56:13 -0300

    Add installation instructions

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,22 @@
-#TODO: Make tag versionable
-TAG="drtools/prefect:spec2vec_mlops_v4"
-SPARK_TAG="drtools/prefect:spec2vec_mlops_spark_v1"
+snap ()
+{
+    echo "-SNAPSHOT.$(git rev-parse --short HEAD)"
+}
 
+#TODO: Make tag versionable
+TAG="drtools/prefect:spec2vec_mlops$(snap)"
+SPARK_TAG="drtools/prefect:spec2vec_mlops_spark$(snap)"
+
+echo "$TAG"
 
 docker build -t $TAG -f default.Dockerfile .
 docker push $TAG
 
-docker build -t$SPARK_TAG -f default.Dockerfile .
-docker push $SPARK_TAG
+echo "Successfully pushed $TAG"
+
+if [ -z "$1" = "spark"]; then
+  echo "Building spark image"
+  docker build -t$SPARK_TAG -f default.Dockerfile .
+  docker push $SPARK_TAG
+
+fi
