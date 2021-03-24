63b14418250f0263a4168a099f595f6f275b9af5
Diana Carvalho <diana@datarevenue.com>
2021-03-24 16:41:36 +0000

    Save words in Feast
    Move data storer to a separate file
    Add create_time to spectrum table. Event timestamp is normal now
    
    MLOPS-142 <#DTT#>

---
+++ spec2vec_mlops/helper_classes/data_storer.py
@@ -0,0 +1,109 @@
+import datetime
+from typing import List
+
+import pandas as pd
+from feast import ValueType, Client, FeatureTable, Entity, Feature, FileSource
+from feast.data_format import ParquetFormat
+from matchms import Spectrum
+from spec2vec import SpectrumDocument
+
+from spec2vec_mlops import config
+
+KEYS = config["cleaned_data"]["necessary_keys"].get(list)
+
+
+class DataStorer:
+    def __init__(self, out_dir: str, feast_core_url: str):
+        self.feast_core_url = feast_core_url
+        self.feature_table_name = "spectrum_info"
+        not_string_features2types = {
+            "mz_list": ValueType.DOUBLE_LIST,
+            "intensity_list": ValueType.DOUBLE_LIST,
+            "precursor_mz": ValueType.FLOAT,
+            "charge": ValueType.INT64,
+            "parent_mass": ValueType.FLOAT,
+            "words": ValueType.DOUBLE_LIST,
+        }
+        string_features2types = {
+            key.lower(): ValueType.STRING
+            for key in KEYS
+            if key.lower() not in not_string_features2types.keys()
+        }
+        self.features2types = {**not_string_features2types, **string_features2types}
+        self.out_dir = out_dir
+
+    def store_cleaned_data(self, data: List[Spectrum]):
+        client = Client(core_url=self.feast_core_url, telemetry=False)
+        if not any(
+            table.name != self.feature_table_name
+            for table in client.list_feature_tables()
+        ):
+            spectrum_info = self._create_spectrum_info_table(client)
+        else:
+            spectrum_info = client.get_feature_table(self.feature_table_name)
+        data_df = self._get_cleaned_data_df(data)
+        client.ingest(spectrum_info, data_df)
+
+    def _create_spectrum_info_table(self, client) -> FeatureTable:
+        spectrum_id = Entity(
+            name="spectrum_id",
+            description="Spectrum identifier",
+            value_type=ValueType.INT64,
+        )
+        features = [
+            Feature(feature, dtype=feature_type)
+            for feature, feature_type in self.features2types.items()
+        ]
+        batch_source = FileSource(
+            file_format=ParquetFormat(),
+            file_url=str(self.out_dir),
+            event_timestamp_column="event_timestamp",
+            created_timestamp_column="created_timestamp",
+        )
+        spectrum_info = FeatureTable(
+            name=self.feature_table_name,
+            entities=["spectrum_id"],
+            features=features,
+            batch_source=batch_source,
+        )
+        client.apply(spectrum_id)
+        client.apply(spectrum_info)
+        return spectrum_info
+
+    def _get_cleaned_data_df(self, data: List[Spectrum]) -> pd.DataFrame:
+        return pd.DataFrame.from_records(
+            [
+                {
+                    "spectrum_id": spectrum.metadata["spectrum_id"],
+                    "mz_list": spectrum.peaks.mz,
+                    "intensity_list": spectrum.peaks.intensities,
+                    "words": [],
+                    **{
+                        key: spectrum.metadata[key]
+                        for key in self.features2types.keys()
+                        if key in spectrum.metadata.keys()
+                    },
+                    "event_timestamp": datetime.datetime.now(),
+                    "created_timestamp": datetime.datetime.now(),
+                }
+                for spectrum in data
+            ]
+        )
+
+    def store_words(self, data: List[SpectrumDocument]):
+        client = Client(core_url=self.feast_core_url, telemetry=False)
+        spectrum_info = self._create_spectrum_info_table(client)
+        data_df = self._get_words_df(data)
+        client.ingest(spectrum_info, data_df)
+
+    def _get_words_df(self, data: List[SpectrumDocument]) -> pd.DataFrame:
+        return pd.DataFrame.from_records(
+            [
+                {
+                    "spectrum_id": document.metadata["spectrum_id"],
+                    "words": document.words,
+                    "event_timestamp": datetime.datetime.now(),
+                }
+                for document in data
+            ]
+        )
---
+++ spec2vec_mlops/test/helper_classes/test_data_storer.py
@@ -0,0 +1,51 @@
+import pytest
+from feast import Client
+
+from spec2vec_mlops import config
+from spec2vec_mlops.helper_classes.data_storer import DataStorer
+from spec2vec_mlops.tasks.convert_to_documents import DocumentConverter
+
+FEAST_CORE_URL = config["feast"]["url"]["local"].get(str)
+
+# pytestmark = pytest.mark.skip(
+#     "These tests can only be run if the Feast docker-compose is up"
+# )
+
+
+@pytest.fixture()
+def data_storer(tmpdir):
+    return DataStorer(f"file://{tmpdir}", FEAST_CORE_URL)
+
+
+@pytest.fixture
+def documents_data(cleaned_data):
+    converter = DocumentConverter()
+    return [converter.convert_to_document(spectrum, 1) for spectrum in cleaned_data]
+
+
+def test_create_spectrum_info_table(data_storer):
+    client = Client(core_url=FEAST_CORE_URL, telemetry=False)
+    data_storer._create_spectrum_info_table(client)
+    assert client.list_feature_tables()[0].name == data_storer.feature_table_name
+
+
+def test_get_cleaned_data_df(data_storer, cleaned_data):
+    spectrum_df = data_storer._get_cleaned_data_df(cleaned_data)
+    assert len(spectrum_df) == len(cleaned_data)
+    assert (
+        len(spectrum_df.columns) == len(data_storer.features2types.keys()) + 3
+    )  # +3 because of spectrum_id, created_timestamp and event_timestamp
+
+
+def test_store_cleaned_data(data_storer, cleaned_data):
+    data_storer.store_cleaned_data(cleaned_data)
+
+
+def test_get_words_df(data_storer, documents_data):
+    words_df = data_storer._get_words_df(documents_data)
+    assert len(words_df) == len(documents_data)
+    assert set(words_df.columns) == {"spectrum_id", "words", "event_timestamp"}
+
+
+def test_store_words(data_storer, documents_data):
+    data_storer.store_words(documents_data)
