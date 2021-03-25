b732b8ddad0c182016b11616f498131d9f0dbea0
cereniyim <ceren@datarevenue.com>
2021-03-25 10:57:56 +0300

    set PYTHONPATH to github repo name, correct image tag in spec2vec_train_pipeline_distributed
    --- don't change below this line ---
    MLOPS-172 #time 22m

--- spec2vec_mlops/flows/training_flow.py
+++ spec2vec_mlops/flows/training_flow.py
@@ -51,7 +51,7 @@ def spec2vec_train_pipeline_distributed(
     """
     custom_confs = {
         "run_config": KubernetesRun(
-            image="drtools/prefect:spec2vec_mlops_v6",
+            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.402f067",
             labels=["dev"],
             service_account_name="prefect-server-serviceaccount",
         ),
