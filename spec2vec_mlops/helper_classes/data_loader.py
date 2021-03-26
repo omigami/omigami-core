79feb2e5c530408b54931ec9c238876e1b38c171
cereniyim <ceren@datarevenue.com>
2021-03-26 11:17:35 +0300

    separate classes from tasks, gather classes under helper_classes directory
    move tests accordingly
    
    --- don't change below this line ---
    MLOPS-180 #time 2h 3m

---
+++ spec2vec_mlops/helper_classes/data_loader.py
@@ -0,0 +1,25 @@
+import logging
+from typing import Dict, List
+from urllib.request import urlopen
+
+import ijson
+
+from spec2vec_mlops import config
+
+KEYS = config["gnps_json"]["necessary_keys"].get(list)
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class DataLoader:
+    def __init__(self):
+        pass
+
+    def load_gnps_json(self, uri: str) -> List[Dict]:
+        uri = urlopen(uri)
+        logger.info(f"Loading data from {uri}... This might take a while.")
+        items = ijson.items(uri, "item", multiple_values=True)
+        results = [{k: item[k] for k in KEYS} for item in items]
+        return results
