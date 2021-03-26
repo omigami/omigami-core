096a438f5015c5747c345ec682769a9b1b20a87d
cereniyim <ceren@datarevenue.com>
2021-03-26 11:42:39 +0300

    rename store_cleaned_task to store_cleaned_data_task
    --- don't change below this line ---
    MLOPS-180 #time 3m

--- spec2vec_mlops/flows/training_flow.py
+++ spec2vec_mlops/flows/training_flow.py
@@ -12,7 +12,7 @@ from spec2vec_mlops.tasks.convert_to_documents import convert_to_documents_task
 from spec2vec_mlops.tasks.load_data import load_data_task
 from spec2vec_mlops.tasks.clean_data import clean_data_task
 from spec2vec_mlops.tasks.store_cleaned_data_and_words import (
-    store_cleaned_task,
+    store_cleaned_data_task,
     store_words_task,
 )
 
@@ -35,7 +35,7 @@ def spec2vec_train_pipeline_local(
         logger.info("Data loading is complete.")
         cleaned = clean_data_task.map(raw)
         logger.info("Data cleaning is complete.")
-        store_cleaned_task(cleaned, feast_source_dir, feast_core_url)
+        store_cleaned_data_task(cleaned, feast_source_dir, feast_core_url)
         documents = convert_to_documents_task.map(cleaned, n_decimals=unmapped(2))
         store_words_task(documents, feast_source_dir, feast_core_url)
     state = flow.run()
@@ -80,7 +80,7 @@ def spec2vec_train_pipeline_distributed(
         logger.info("Data loading is complete.")
         cleaned = clean_data_task.map(raw)
         logger.info("Data cleaning is complete.")
-        store_cleaned_task(cleaned, feast_source_dir, feast_core_url)
+        store_cleaned_data_task(cleaned, feast_source_dir, feast_core_url)
         documents = convert_to_documents_task.map(cleaned, n_decimals=unmapped(2))
         store_words_task(documents, feast_source_dir, feast_core_url)
         # encoded = encode_training_data_task(documents)
--- spec2vec_mlops/tasks/store_cleaned_data_and_words.py
+++ spec2vec_mlops/tasks/store_cleaned_data_and_words.py
@@ -9,7 +9,7 @@ from spec2vec_mlops.helper_classes.data_storer import DataStorer
 
 
 @task(max_retries=3, retry_delay=datetime.timedelta(seconds=10))
-def store_cleaned_task(data: List[Spectrum], out_dir: str, feast_core_url: str):
+def store_cleaned_data_task(data: List[Spectrum], out_dir: str, feast_core_url: str):
     ds = DataStorer(out_dir, feast_core_url)
     ds.store_cleaned_data(data)
 
