f52b70d2a03d520c44324b4f13bf4cf2a0804d43
cereniyim <ceren@datarevenue.com>
2021-03-26 12:48:18 +0300

    fixes after revert commit
    --- don't change below this line ---
    MLOPS-180 #time 3m

--- spec2vec_mlops/tasks/store_words.py
+++ spec2vec_mlops/tasks/store_words.py
@@ -7,12 +7,6 @@ from spec2vec_mlops.helper_classes.data_storer import DataStorer
 
 
 @task(max_retries=3, retry_delay=datetime.timedelta(seconds=10))
-def store_cleaned_data_task(data: List[Spectrum], out_dir: str, feast_core_url: str):
-    ds = DataStorer(out_dir, feast_core_url)
-    ds.store_cleaned_data(data)
-
-
-@task(max_retries=3, retry_delay=datetime.timedelta(seconds=10))
 def store_words_task(data: List[SpectrumDocument], out_dir: str, feast_core_url: str):
     ds = DataStorer(out_dir, feast_core_url)
     ds.store_words(data)
