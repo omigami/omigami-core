9fd956044ac3e319e857a300a60f8902083c6f43
cereniyim <ceren@datarevenue.com>
2021-03-26 11:24:58 +0300

    black-format all files
    --- don't change below this line ---
    MLOPS-180 #time 5m

--- spec2vec_mlops/test/helper_classes/test_data_storer.py
+++ spec2vec_mlops/test/helper_classes/test_data_storer.py
@@ -23,7 +23,10 @@ def documents_data(cleaned_data):
 
 def test_create_spectrum_info_table(data_storer):
     data_storer._create_spectrum_info_table()
-    assert data_storer.client.list_feature_tables()[0].name == data_storer.feature_table_name
+    assert (
+        data_storer.client.list_feature_tables()[0].name
+        == data_storer.feature_table_name
+    )
 
 
 def test_get_cleaned_data_df(data_storer, cleaned_data):
