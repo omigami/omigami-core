17168f7ead7285cfeae6a56b6ab80d0c4e0dc2ab
cereniyim <ceren@datarevenue.com>
2021-03-30 11:39:17 +0300

    update image to most recent commit, test flow on cluster
    --- don't change below this line ---
    MLOPS-144 #time 20m

--- spec2vec_mlops/flows/training_flow.py
+++ spec2vec_mlops/flows/training_flow.py
@@ -97,7 +97,7 @@ def spec2vec_train_pipeline_distributed(
     """
     custom_confs = {
         "run_config": KubernetesRun(
-            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.e348dca",
+            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.58dcc20",
             labels=["dev"],
             service_account_name="prefect-server-serviceaccount",
         ),
