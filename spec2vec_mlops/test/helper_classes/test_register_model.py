c6c49cf6f74f294f8dfdd350ca5e217d032615d8
cereniyim <ceren@datarevenue.com>
2021-03-30 14:19:24 +0300

    delete trained_model fixture in test_register_model.py
    because we have it as a test asset now
    
    --- don't change below this line ---
    MLOPS-144 #time 3m

--- spec2vec_mlops/test/helper_classes/test_register_model.py
+++ spec2vec_mlops/test/helper_classes/test_register_model.py
@@ -1,20 +1,13 @@
 import os
-import pytest
+
 from spec2vec_mlops.helper_classes.model_register import Model, ModelRegister
-from spec2vec_mlops.helper_classes.model_trainer import ModelTrainer
 
 
-@pytest.fixture
-def trained_model(documents_data):
-    model_trainer = ModelTrainer()
-    return model_trainer.train_model(documents_data, iterations=10, window=5)
-
-
-def test_register_model(trained_model, tmpdir):
+def test_register_model(word2vec_model, tmpdir):
     path = f"{tmpdir}/mlflow/"
     model_register = ModelRegister(f"file:/{path}")
     model_register.register_model(
-        Model(trained_model), "experiment", f"{path}/model", n_decimals=2
+        Model(word2vec_model), "experiment", f"{path}/model", n_decimals=2
     )
     assert os.path.exists(f"{path}/model/python_model.pkl")
 
