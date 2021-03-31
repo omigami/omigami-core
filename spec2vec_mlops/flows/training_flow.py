d9f93e20292753aa38e88810cdca281ae3c845fd
Pat T <user@example.com>
2021-03-31 17:51:13 -0400

    load data from feast when training a model
    MLOPS-178 #time 1m

--- spec2vec_mlops/flows/training_flow.py
+++ spec2vec_mlops/flows/training_flow.py
@@ -83,9 +83,11 @@ def spec2vec_train_pipeline_distributed(
         cleaned = clean_data_task.map(raw)
         logger.info("Data cleaning is complete.")
         store_cleaned_data_task(cleaned, feast_source_dir, feast_core_url)
+
+        # TODO: this step should use data from feast
         documents = convert_to_documents_task.map(cleaned, n_decimals=unmapped(2))
         store_documents_task(documents, feast_source_dir, feast_core_url)
-        model = train_model_task(documents, iterations, window)
+        model = train_model_task(feast_core_url, iterations, window)
         register_model_task(
             mlflow_server_uri,
             model,
--- spec2vec_mlops/tasks/train_model.py
+++ spec2vec_mlops/tasks/train_model.py
@@ -5,15 +5,18 @@ from gensim.models import Word2Vec
 from prefect import task
 from spec2vec import SpectrumDocument
 
+from spec2vec_mlops.helper_classes.feature_loader import FeatureLoader
 from spec2vec_mlops.helper_classes.model_trainer import ModelTrainer
 
 
 @task(max_retries=3, retry_delay=datetime.timedelta(seconds=10))
 def train_model_task(
-    documents: List[SpectrumDocument],
+    feast_core_url: str,
     iterations: int = 25,
     window: int = 500,
 ) -> Word2Vec:
+    feature_loader = FeatureLoader(feast_core_url)
+    documents = feature_loader.load_documents()
     model_trainer = ModelTrainer()
     model = model_trainer.train_model(documents, iterations, window)
     return model
