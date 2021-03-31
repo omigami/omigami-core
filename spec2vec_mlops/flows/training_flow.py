7bcef7b2d878104b06c3a3f892081fc6043d65ab
cereniyim <ceren@datarevenue.com>
2021-03-31 18:05:46 +0300

    update and test distributed pipeline
    --- don't change below this line ---
    MLOPS-184 #time 15m

--- spec2vec_mlops/flows/training_flow.py
+++ spec2vec_mlops/flows/training_flow.py
@@ -33,7 +33,7 @@ MLFLOW_SERVER_REMOTE = config["mlflow"]["url"]["remote"].get(str)
 def spec2vec_train_pipeline_distributed(
     source_uri: str = SOURCE_URI_PARTIAL_GNPS,  # TODO when running in prod set to SOURCE_URI_COMPLETE_GNPS
     api_server: str = API_SERVER_REMOTE,
-    project_name: str = "spec2vec-mlops-project-store-emdeddings",
+    project_name: str = "spec2vec-mlops-project-store-emdeddings-w-run_id",
     feast_source_dir: str = "s3://dr-prefect/spec2vec-training-flow/feast",
     feast_core_url: str = FEAST_CORE_URL_REMOTE,
     n_decimals: int = 2,
@@ -70,7 +70,7 @@ def spec2vec_train_pipeline_distributed(
     """
     custom_confs = {
         "run_config": KubernetesRun(
-            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.7db82c2",
+            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.1c6f682",
             labels=["dev"],
             service_account_name="prefect-server-serviceaccount",
         ),
@@ -87,7 +87,7 @@ def spec2vec_train_pipeline_distributed(
         documents = convert_to_documents_task.map(cleaned, n_decimals=unmapped(2))
         store_documents_task(documents, feast_source_dir, feast_core_url)
         model = train_model_task(documents, iterations, window)
-        register_model_task(
+        run_id = register_model_task(
             mlflow_server_uri,
             model,
             project_name,
@@ -101,7 +101,7 @@ def spec2vec_train_pipeline_distributed(
             unmapped(intensity_weighting_power),
             unmapped(allowed_missing_percentage),
         )
-        store_embeddings_task(documents, embeddings, feast_source_dir, feast_core_url)
+        store_embeddings_task(documents, embeddings, run_id, feast_source_dir, feast_core_url)
     client = Client(api_server=api_server)
     client.create_project(project_name)
     training_flow_id = client.register(
