b403b8c1a63b5ad03eee3e9e6f92845102342d68
cereniyim <ceren@datarevenue.com>
2021-04-05 11:18:15 +0300

    update and test distributed flow
    flow has failed due to change in Entity.value_type from INT64 to STRING
    
    --- don't change below this line ---
    MLOPS-184 #time 22m

--- spec2vec_mlops/flows/training_flow.py
+++ spec2vec_mlops/flows/training_flow.py
@@ -33,7 +33,7 @@ MLFLOW_SERVER_REMOTE = config["mlflow"]["url"]["remote"].get(str)
 def spec2vec_train_pipeline_distributed(
     source_uri: str = SOURCE_URI_PARTIAL_GNPS,  # TODO when running in prod set to SOURCE_URI_COMPLETE_GNPS
     api_server: str = API_SERVER_REMOTE,
-    project_name: str = "spec2vec-mlops-project-spec2vec-embeddings",
+    project_name: str = "spec2vec-mlops-project-spec2vec-store-embeddings-in-diff-tables",
     feast_source_dir: str = "s3://dr-prefect/spec2vec-training-flow/feast",
     feast_core_url: str = FEAST_CORE_URL_REMOTE,
     n_decimals: int = 2,
@@ -70,7 +70,7 @@ def spec2vec_train_pipeline_distributed(
     """
     custom_confs = {
         "run_config": KubernetesRun(
-            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.9ea7556",
+            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.d99898c",
             labels=["dev"],
             service_account_name="prefect-server-serviceaccount",
         ),
@@ -103,9 +103,7 @@ def spec2vec_train_pipeline_distributed(
             unmapped(intensity_weighting_power),
             unmapped(allowed_missing_percentage),
         )
-        store_embeddings_task(
-            documents, embeddings, run_id, feast_source_dir, feast_core_url
-        )
+        store_embeddings_task(embeddings, run_id, feast_source_dir, feast_core_url)
     client = Client(api_server=api_server)
     client.create_project(project_name)
     training_flow_id = client.register(
