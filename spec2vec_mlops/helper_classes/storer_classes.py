c8cbdadb2dfffdf6af67be85c2c901071aa2ec21
cereniyim <ceren@datarevenue.com>
2021-04-05 12:54:58 +0300

    reflect PR remarks in storer_classes.py: * add losses to store in spectrum_info table * move _convert_create_time to Storer class, add typing hint
    --- don't change below this line ---
    MLOPS-184 #time 1h 19m

--- spec2vec_mlops/helper_classes/storer_classes.py
+++ spec2vec_mlops/helper_classes/storer_classes.py
@@ -16,6 +16,7 @@ KEYS = config["cleaned_data"]["necessary_keys"].get(list)
 not_string_features2types = {
     "mz_list": ValueType.DOUBLE_LIST,
     "intensity_list": ValueType.DOUBLE_LIST,
+    "losses": ValueType.DOUBLE_LIST,
     "precursor_mz": ValueType.FLOAT,
     "charge": ValueType.INT64,
     "parent_mass": ValueType.FLOAT,
@@ -72,6 +73,13 @@ class Storer(BaseStorer):
         self.client.apply(feature_table)
         return feature_table
 
+    @staticmethod
+    def _convert_create_time(create_time: str) -> datetime:
+        if create_time:
+            return datetime.strptime(create_time, "%Y-%m-%d %H:%M:%S.%f")
+        else:
+            return datetime.now()
+
 
 class SpectrumStorer(Storer):
     def __init__(self, out_dir: str, feast_core_url: str, feature_table_name: str):
@@ -97,6 +105,7 @@ class SpectrumStorer(Storer):
                     "spectrum_id": spectrum.metadata["spectrum_id"],
                     "mz_list": spectrum.peaks.mz,
                     "intensity_list": spectrum.peaks.intensities,
+                    "losses": spectrum.losses,
                     **{
                         key: spectrum.metadata[key]
                         for key in self.features2types.keys()
@@ -110,13 +119,6 @@ class SpectrumStorer(Storer):
                 for spectrum in data
             ]
         )
-
-    @staticmethod
-    def _convert_create_time(create_time: str):
-        if create_time:
-            return datetime.strptime(create_time, "%Y-%m-%d %H:%M:%S.%f")
-        else:
-            return datetime.now()
 
 
 class DocumentStorer(Storer):
@@ -154,13 +156,6 @@ class DocumentStorer(Storer):
                 for document in data
             ]
         )
-
-    @staticmethod
-    def _convert_create_time(create_time: str):
-        if create_time:
-            return datetime.strptime(create_time, "%Y-%m-%d %H:%M:%S.%f")
-        else:
-            return datetime.now()
 
 
 class EmbeddingStorer(Storer):
