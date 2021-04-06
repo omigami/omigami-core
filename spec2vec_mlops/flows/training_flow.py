5db45ad1ce23acf2668f68e4238e23e86b1902c0
Pat Thontirawong <pat@datarevenue.com>
2021-04-06 16:56:23 -0400

    update image tag
    MLOPS-178 #time 1m

--- spec2vec_mlops/flows/training_flow.py
+++ spec2vec_mlops/flows/training_flow.py
@@ -66,7 +66,7 @@ def spec2vec_train_pipeline_distributed(
     """
     custom_confs = {
         "run_config": KubernetesRun(
-            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.f7487d6",
+            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.30b68c1",
             labels=["dev"],
             service_account_name="prefect-server-serviceaccount",
             env={
