a97a2419c6f29066c6c8ec2c57bf3e334b039127
dianacarvalho1 <diana@datarevenue.com>
2021-04-07 13:36:01 +0100

    Update image tag
    --- don't change below this line ---
    MLOPS-145 #time 5m

--- spec2vec_mlops/flows/training_flow.py
+++ spec2vec_mlops/flows/training_flow.py
@@ -75,7 +75,7 @@ def spec2vec_train_pipeline_distributed(
     """
     custom_confs = {
         "run_config": KubernetesRun(
-            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.4976da6",
+            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.d455e84",
             labels=["dev"],
             service_account_name="prefect-server-serviceaccount",
         ),
