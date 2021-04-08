f4132cc6aeba763efb1d8dbb9d20c77e6bfa1814
cereniyim <ceren@datarevenue.com>
2021-04-08 18:32:29 +0300

    implement alternative data loading with URLGrabber.urlgrab: * save file to s3 after loading * read json from saved file
    --- don't change below this line ---
    MLOPS-164 #time 5h 7m

--- spec2vec_mlops/helper_classes/data_loader.py
+++ spec2vec_mlops/helper_classes/data_loader.py
@@ -5,8 +5,13 @@ from urllib.request import urlopen
 import ijson
 
 from spec2vec_mlops import config
+from pathlib import Path
+from urlgrabber.grabber import URLGrabber
+from drfs.filesystems import get_fs
+from uuid import uuid4
 
 KEYS = config["gnps_json"]["necessary_keys"].get(list)
+SOURCE_URI_PARTIAL_GNPS = config["gnps_json"]["uri"]["partial"].get(str)
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -14,9 +19,30 @@ logger = logging.getLogger(__name__)
 
 
 class DataLoader:
+    def __init__(self, uri: str = SOURCE_URI_PARTIAL_GNPS):
+        self.uri = uri
+
     def load_gnps_json(self, uri: str) -> List[Dict]:
-        uri = urlopen(uri)
         logger.info(f"Loading data from {uri}... This might take a while.")
-        items = ijson.items(uri, "item", multiple_values=True)
+        response = urlopen(uri)
+        items = ijson.items(response, "item", multiple_values=True)
         results = [{k: item[k] for k in KEYS} for item in items]
+        return results
+
+    def _save(self, out_dir: Path = None):
+        g = URLGrabber(retry=0, reget="simple")  # 0 retry until you get the file, reget is to resume the download
+        logger.info(f"Loading data from {self.uri}... This might take a while.")
+        if out_dir is None:
+            out_dir = "s3://dr-prefect/spec2vec-data/"  # TODO: change this to prod bucket
+        file_id = str(uuid4())
+        filename = f"{str(out_dir)}/{file_id}.json"
+        filename = g.urlgrab(url=self.uri, filename=filename)
+        return filename
+
+    def load(self, out_dir: Path = None):
+        in_file = self._save(out_dir)
+        fs = get_fs(in_file)
+        with fs.open(in_file, "rb") as f:
+            items = ijson.items(f, "item", multiple_values=True)
+            results = [{k: item[k] for k in KEYS} for item in items]
         return results
--- spec2vec_mlops/test/helper_classes/test_data_loader.py
+++ spec2vec_mlops/test/helper_classes/test_data_loader.py
@@ -31,3 +31,61 @@ def test_load_gnps_json_with_local_uri(gnps_small_json):
         assert isinstance(res, dict)
         for k in KEYS:
             assert k in res
+
+@pytest.mark.longrun
+@pytest.mark.parametrize(
+    "uri",
+    [
+        "https://raw.githubusercontent.com/MLOps-architecture/share/main/test_data/SMALL_GNPS.json",
+        "https://gnps-external.ucsd.edu/gnpslibrary/ALL_GNPS.json",
+    ],
+)
+def test_save_web_uri(uri, tmpdir):
+    dl = DataLoader(uri=uri)
+    res = dl._save(out_dir=tmpdir)
+
+    assert isinstance(res, str)
+    assert res.split(".")[1] == "json"
+
+
+@pytest.mark.longrun
+@pytest.mark.parametrize(
+    "uri",
+    [
+        "https://raw.githubusercontent.com/MLOps-architecture/share/main/test_data/SMALL_GNPS.json",
+        "https://gnps-external.ucsd.edu/gnpslibrary/ALL_GNPS.json",
+    ],
+)
+def test_load_web_uri(uri, tmpdir):
+    dl = DataLoader(uri=uri)
+
+    for res in dl.load(out_dir=tmpdir):
+        assert isinstance(res, dict)
+        for k in KEYS:
+            assert k in res
+
+
+def test_save_local_uri(gnps_small_json, tmpdir):
+    dl = DataLoader(uri=gnps_small_json)
+    res = dl._save(out_dir=tmpdir)
+
+    assert isinstance(res, str)
+    assert res.split(".")[1] == "json"
+
+
+def test_load_local_uri(gnps_small_json, tmpdir):
+    dl = DataLoader(uri=gnps_small_json)
+
+    for res in dl.load(out_dir=tmpdir):
+        assert isinstance(res, dict)
+        for k in KEYS:
+            assert k in res
+
+
+def test_load_local_uri_save_to_s3(gnps_small_json):
+    dl = DataLoader(uri=gnps_small_json)
+
+    for res in dl.load():
+        assert isinstance(res, dict)
+        for k in KEYS:
+            assert k in res
