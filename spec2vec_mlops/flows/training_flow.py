2468b30f2c6169ab69ec16fbebd72543fbce78fa
dianacarvalho1 <diana@datarevenue.com>
2021-04-14 10:54:09 +0100

    endpoint type rest
    --- don't change below this line ---
    MLOPS-189 #time 13m

--- spec2vec_mlops/flows/training_flow.py
+++ spec2vec_mlops/flows/training_flow.py
@@ -75,7 +75,7 @@ def spec2vec_train_pipeline_distributed(
     """
     custom_confs = {
         "run_config": KubernetesRun(
-            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.663e6ba",
+            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.a2224ed",
             labels=["dev"],
             service_account_name="prefect-server-serviceaccount",
         ),
