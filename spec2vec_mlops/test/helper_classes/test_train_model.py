94c0c8ccafa34419da92e5819e6e4b76bc2c672c
dianacarvalho1 <diana@datarevenue.com>
2021-04-15 10:45:49 +0100

    Use epochs instead of iter in model
    --- don't change below this line ---
    MLOPS-189 #time 9m

--- spec2vec_mlops/test/helper_classes/test_train_model.py
+++ spec2vec_mlops/test/helper_classes/test_train_model.py
@@ -1,4 +1,5 @@
 from gensim.models import Word2Vec
+
 from spec2vec_mlops.helper_classes.model_trainer import ModelTrainer
 
 
@@ -6,5 +7,5 @@ def test_train_model(documents_data):
     model_trainer = ModelTrainer()
     model = model_trainer.train_model(documents_data, iterations=10, window=5)
     assert isinstance(model, Word2Vec)
-    assert model.iter == 10
+    assert model.epochs == 10
     assert model.window == 5
