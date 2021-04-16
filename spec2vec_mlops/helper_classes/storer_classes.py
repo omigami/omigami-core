d2a3e55be7d361d3c7ecea875c5c08f9199513c7
Pat Thontirawong <pat@datarevenue.com>
2021-04-16 13:54:17 -0400

    read all ids offline if online fails
    MLOPS-164 #time 5m

--- spec2vec_mlops/helper_classes/storer_classes.py
+++ spec2vec_mlops/helper_classes/storer_classes.py
@@ -11,6 +11,7 @@ from spec2vec_mlops import config
 from spec2vec_mlops.entities.embedding import Embedding
 from spec2vec_mlops.entities.feast_spectrum_document import FeastSpectrumDocument
 from spec2vec_mlops.helper_classes.base_storer import BaseStorer
+from spec2vec_mlops.helper_classes.exception import StorerLoadError
 from spec2vec_mlops.helper_classes.feast_dgw import FeastDataGateway
 
 TIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
@@ -43,7 +44,10 @@ class SpectrumIDStorer(BaseStorer):
         )
 
     def store(self, data: List[str]):
-        existing_ids = self.read_online() or []
+        try:
+            existing_ids = self.read_online() or self.read_offline()
+        except StorerLoadError:
+            existing_ids = []
         new_ids = list(set(data).difference(existing_ids))
         all_ids = [*existing_ids, *new_ids]
         data_df = self._get_data_df(all_ids)
