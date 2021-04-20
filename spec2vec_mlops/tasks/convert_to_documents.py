bc127bdb63f5c39d04bef41bdf0cf7d2a287c23b
Pat Thontirawong <pat@datarevenue.com>
2021-04-20 15:48:11 -0400

    limit document read_online chunksize to 100
    MLOPS-146 #time 2h 48m

--- spec2vec_mlops/tasks/convert_to_documents.py
+++ spec2vec_mlops/tasks/convert_to_documents.py
@@ -16,6 +16,18 @@ logger = logging.getLogger(__name__)
 
 @task(max_retries=3, retry_delay=datetime.timedelta(seconds=10))
 def convert_to_documents_task(spectrum_ids: List[str], n_decimals: int) -> List[str]:
+    chunksize = 100  # capped by document_storer.read_online gRPC message size limit
+    ids_chunks = [
+        spectrum_ids[i : i + chunksize] for i in range(0, len(spectrum_ids), chunksize)
+    ]
+    return [
+        output_id
+        for ids_chunk in ids_chunks
+        for output_id in _read_and_convert(ids_chunk, n_decimals)
+    ]
+
+
+def _read_and_convert(spectrum_ids: List[str], n_decimals: int) -> List[str]:
     spectrum_storer = SpectrumStorer("spectrum_info")
     all_spectra = spectrum_storer.read_online(spectrum_ids)
     if all_spectra:
