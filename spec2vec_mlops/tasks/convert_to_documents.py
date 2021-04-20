795fc13799386832b281914ed69538a66784f15d
dianacarvalho1 <diana@datarevenue.com>
2021-04-20 17:31:11 +0100

    Add logs to see number of spectra
    --- don't change below this line ---
    MLOPS-214 #time 2m

--- spec2vec_mlops/tasks/convert_to_documents.py
+++ spec2vec_mlops/tasks/convert_to_documents.py
@@ -1,19 +1,25 @@
 import datetime
+import logging
 from typing import List
 
 from prefect import task
 
+from spec2vec_mlops.helper_classes.document_converter import DocumentConverter
 from spec2vec_mlops.helper_classes.storer_classes import (
     SpectrumStorer,
     DocumentStorer,
 )
-from spec2vec_mlops.helper_classes.document_converter import DocumentConverter
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 @task(max_retries=3, retry_delay=datetime.timedelta(seconds=10))
 def convert_to_documents_task(spectrum_ids: List[str], n_decimals: int) -> List[str]:
     spectrum_storer = SpectrumStorer("spectrum_info")
     all_spectra = spectrum_storer.read_online(spectrum_ids)
+    if all_spectra:
+        logger.info(f"Using previously downloaded data")
     document_converter = DocumentConverter()
     result = [
         document_converter.convert_to_document(spectrum, n_decimals)
