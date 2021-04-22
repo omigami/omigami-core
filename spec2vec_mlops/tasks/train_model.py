178dc2235654e54fb02b6582e2382b821cbee690
Pierre Merckaert <pierre@datarevenue.com>
2021-04-22 13:35:15 +0200

    pass iterable to word2vec for training
    --- don't change below this line ---
    MLOPS-212 #time 3h 28m

--- spec2vec_mlops/tasks/train_model.py
+++ spec2vec_mlops/tasks/train_model.py
@@ -1,13 +1,30 @@
 import datetime
+from typing import Union, List
 
+import gensim
 from gensim.models import Word2Vec
 from prefect import task
+from spec2vec.model_building import (
+    set_spec2vec_defaults,
+    learning_rates_to_gensim_style,
+)
+from spec2vec.utils import TrainingProgressLogger, ModelSaver
 
 from spec2vec_mlops.helper_classes.storer_classes import (
     SpectrumIDStorer,
     DocumentStorer,
 )
-from spec2vec_mlops.helper_classes.model_trainer import ModelTrainer
+
+
+class MyCorpus:
+    """An iterator that yields spectra (lists of str)."""
+
+    def __iter__(self):
+        ids_storer = SpectrumIDStorer("spectrum_ids_info")
+        document_storer = DocumentStorer("document_info")
+        all_spectrum_ids = ids_storer.read_online()
+        for spectrum_id in all_spectrum_ids:
+            yield document_storer.read_online([spectrum_id])[0]
 
 
 @task(max_retries=3, retry_delay=datetime.timedelta(seconds=10))
@@ -15,10 +32,34 @@ def train_model_task(
     iterations: int = 25,
     window: int = 500,
 ) -> Word2Vec:
-    ids_storer = SpectrumIDStorer("spectrum_ids_info")
-    document_storer = DocumentStorer("document_info")
-    all_spectrum_ids = ids_storer.read_online()
-    documents = document_storer.read_online(all_spectrum_ids)
-    model_trainer = ModelTrainer()
-    model = model_trainer.train_model(documents, iterations, window)
+    callbacks, settings = spec2vec_settings(iterations=iterations, window=window)
+    documents = MyCorpus()
+    model = gensim.models.Word2Vec(sentences=documents, callbacks=callbacks, **settings)
     return model
+
+
+def spec2vec_settings(
+    iterations: Union[List[int], int],
+    filename: str = None,
+    progress_logger: bool = True,
+    **settings
+):
+    settings = set_spec2vec_defaults(**settings)
+
+    num_of_epochs = max(iterations) if isinstance(iterations, list) else iterations
+
+    # Convert spec2vec style arguments to gensim style arguments
+    settings = learning_rates_to_gensim_style(num_of_epochs, **settings)
+
+    # Set callbacks
+    callbacks = []
+    if progress_logger:
+        training_progress_logger = TrainingProgressLogger(num_of_epochs)
+        callbacks.append(training_progress_logger)
+    if filename:
+        if isinstance(iterations, int):
+            iterations = [iterations]
+        model_saver = ModelSaver(num_of_epochs, iterations, filename)
+        callbacks.append(model_saver)
+
+    return callbacks, settings
--- spec2vec_mlops/test/helper_classes/test_train_model.py
+++ spec2vec_mlops/test/helper_classes/test_train_model.py
@@ -1,11 +1,29 @@
-from gensim.models import Word2Vec
+from typing import Iterable
 
-from spec2vec_mlops.helper_classes.model_trainer import ModelTrainer
+from spec2vec.utils import TrainingProgressLogger
+
+from spec2vec_mlops.entities.feast_spectrum_document import FeastSpectrumDocument
+from spec2vec_mlops.tasks.train_model import MyCorpus, spec2vec_settings
 
 
-def test_train_model(documents_data):
-    model_trainer = ModelTrainer()
-    model = model_trainer.train_model(documents_data, iterations=10, window=5)
-    assert isinstance(model, Word2Vec)
-    assert model.epochs == 10
-    assert model.window == 5
+def test_iterable_corpus():
+    corpus = MyCorpus()
+    all_sentences = 0
+    all_words = 0
+    for sentence in corpus:
+        all_sentences += 1
+        assert isinstance(sentence, FeastSpectrumDocument)
+        for word in sentence:
+            all_words += 1
+            assert word.startswith("peak@")
+
+    assert isinstance(corpus, Iterable)
+    assert all_sentences == 100
+    assert all_words == 33488
+
+
+def test_spec2vec_settings():
+    iterations = 5
+    callbacks, settings = spec2vec_settings(iterations=iterations)
+    assert isinstance(callbacks[0], TrainingProgressLogger)
+    assert settings["iter"] == iterations
