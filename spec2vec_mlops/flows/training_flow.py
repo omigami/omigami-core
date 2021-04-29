d9d2d8c88eda9507faee90d09855654c37144227
Pierre Merckaert <pierre@datarevenue.com>
2021-04-29 14:48:58 +0200

    change imagePullPolicy to Always
    #time 1h 32m

--- spec2vec_mlops/flows/training_flow.py
+++ spec2vec_mlops/flows/training_flow.py
@@ -78,7 +78,7 @@ def spec2vec_train_pipeline_distributed(
     """
     custom_confs = {
         "run_config": KubernetesRun(
-            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.3c7cf0c",
+            image="drtools/prefect:spec2vec_mlops-SNAPSHOT.816930c",
             job_template_path="./spec2vec_mlops/job_spec.yaml",
             labels=["dev"],
             service_account_name="prefect-server-serviceaccount",
