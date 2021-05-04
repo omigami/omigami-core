be6dd10429c89a1dbadd5c9d2e24ec9c31896ce0
czanella <carlos@datarevenue.com>
2021-05-04 16:27:08 +0200

    Remove previous training flow test

--- spec2vec_mlops/test/flows/test_training_flow.py
+++ spec2vec_mlops/test/flows/test_training_flow.py
@@ -1,23 +1,13 @@
 import os
 from pathlib import Path
-from typing import Union
 
 import pytest
 from drfs.filesystems import get_fs
-from prefect import Flow, unmapped, case, task
-from prefect.engine.state import State
+from prefect import task
 
 import spec2vec_mlops.flows.training_flow
 from spec2vec_mlops import config
 from spec2vec_mlops.flows.training_flow import build_training_flow
-from spec2vec_mlops.gateways.input_data_gateway import FSInputDataGateway
-from spec2vec_mlops.tasks.check_condition import check_condition
-from spec2vec_mlops.tasks.clean_data import clean_data_task
-from spec2vec_mlops.tasks.download_data import DownloadData
-from spec2vec_mlops.tasks.load_data import LoadData
-from spec2vec_mlops.tasks.make_embeddings import make_embeddings_task
-from spec2vec_mlops.tasks.register_model import register_model_task
-from spec2vec_mlops.tasks.train_model import train_model_task
 
 SOURCE_URI_PARTIAL_GNPS = config["gnps_json"]["uri"]["partial"]
 os.chdir(Path(__file__).parents[3])
@@ -26,67 +16,6 @@ os.chdir(Path(__file__).parents[3])
 @task()
 def mock_task(a=None, b=None, c=None, **kwargs):
     pass
-
-
-def spec2vec_train_pipeline_local(
-    source_uri: str,
-    download_out_dir: str,
-    n_decimals: int,
-    save_model_path: str,
-    mlflow_server_uri: str,
-    experiment_name: str,
-    iterations: int = 25,
-    window: int = 500,
-    intensity_weighting_power: Union[float, int] = 0.5,
-    allowed_missing_percentage: Union[float, int] = 5.0,
-) -> State:
-    with Flow("flow") as flow:
-        gtw = FSInputDataGateway()
-        file_path = DownloadData(gtw)(source_uri, download_out_dir, "test-ds")
-        raw_chunks = LoadData(gtw)(file_path, chunk_size=5000)
-        all_spectrum_ids_chunks = clean_data_task.map(
-            raw_chunks, n_decimals=unmapped(2)
-        )
-
-        with case(check_condition(all_spectrum_ids_chunks), True):
-            model = train_model_task(iterations, window)
-            run_id = register_model_task(
-                mlflow_server_uri,
-                model,
-                experiment_name,
-                save_model_path,
-                n_decimals,
-                intensity_weighting_power,
-                allowed_missing_percentage,
-            )
-        _ = make_embeddings_task.map(
-            unmapped(model),
-            all_spectrum_ids_chunks,
-            unmapped(run_id),
-            unmapped(n_decimals),
-            unmapped(intensity_weighting_power),
-            unmapped(allowed_missing_percentage),
-        )
-    state = flow.run()
-    return state
-
-
-@pytest.mark.skipif(
-    os.getenv("SKIP_REDIS_TEST", True),
-    reason="It can only be run if the Redis is up",
-)
-def test_spec2vec_train_pipeline_local(tmpdir):
-    state = spec2vec_train_pipeline_local(
-        source_uri=SOURCE_URI_PARTIAL_GNPS,
-        download_out_dir=tmpdir,
-        n_decimals=2,
-        iterations=10,
-        window=5,
-        save_model_path=f"{tmpdir}/mflow",
-        mlflow_server_uri=f"{tmpdir}/mlflow/",
-        experiment_name="experiment",
-    )
-    assert state.is_successful()
 
 
 def test_training_flow():
