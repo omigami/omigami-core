6b390d2cd461aa2e47040b3f588d61bffd230aab
czanella <carlos@datarevenue.com>
2021-05-26 09:54:18 +0200

    Move config loading to config.py

--- spec2vec_mlops/config.py
+++ spec2vec_mlops/config.py
@@ -1,7 +1,13 @@
+import os
 from datetime import datetime
 from pathlib import Path
 
-from spec2vec_mlops import default_configs
+import yaml
+
+with open(
+    os.path.join(os.path.dirname(__file__), "config_default.yaml")
+) as yaml_config_file:
+    default_configs = yaml.safe_load(yaml_config_file)
 
 ROOT_DIR = Path(__file__).parents[0]
 
