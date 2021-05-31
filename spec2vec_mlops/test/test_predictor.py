72aa29330ae0d79ec2ff990d7828aff21c197728
Pierre <pierre@datarevenue.com>
2021-05-31 15:22:52 +0100

    mark skipif test_local_predictions in CI
    #time 8m

--- spec2vec_mlops/test/test_predictor.py
+++ spec2vec_mlops/test/test_predictor.py
@@ -147,13 +147,17 @@ def test_get_best_matches(model, embeddings):
 
 
 @pytest.mark.skipif(
-    os.getenv("SKIP_REDIS_TEST", True) or os.getenv("CI", "False").title(),
+    os.getenv("SKIP_REDIS_TEST", True),
     reason="It can only be run if the Redis is up",
 )
-# @pytest.mark.skipif(
-#     not os.path.exists(str(ASSETS_DIR / "full_data/test_model.pkl")),
-#     reason="test_model.pkl is git ignored",
-# )
+@pytest.mark.skipif(
+    not os.path.exists(str(ASSETS_DIR / "full_data/test_model.pkl")),
+    reason="test_model.pkl is git ignored",
+)
+@pytest.mark.skipif(
+    os.getenv("CI", "False").title(),
+    reason="test_model.pkl is git ignored",
+)
 def test_local_predictions(small_payload, big_payload, spectra_and_embeddings_stored):
     path = str(ASSETS_DIR / "full_data/test_model.pkl")
 
