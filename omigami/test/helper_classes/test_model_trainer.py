ecba5836e7cf0ef89bc1d4669956aaa7d284f16f
bernardolk <bernardo.knackfuss@gmail.com>
2021-06-03 17:23:43 -0300

    fixed tests that got broken
    .
    --- don't change below this line ---
    MLOPS-218 <#DTT#>`

--- omigami/test/helper_classes/test_model_trainer.py
+++ omigami/test/helper_classes/test_model_trainer.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+from unittest.mock import Mock
 
 import gensim
 import pytest
@@ -8,6 +9,7 @@ from spec2vec.utils import TrainingProgressLogger
 
 from omigami.config import DOCUMENT_HASHES
 from omigami.gateways.redis_spectrum_gateway import RedisSpectrumDataGateway
+from omigami.helper_classes.train_logger import CustomTrainingProgressLogger
 from omigami.tasks.train_model import TrainModel
 
 redis_db = factories.redisdb("redis_nooproc")
@@ -27,8 +29,12 @@ def documents_stored(redis_db, cleaned_data, documents_data):
 
 def test_spec2vec_settings():
     epochs = 5
-    callbacks, settings = TrainModel._create_spec2vec_settings(epochs=epochs, window=10)
-    assert isinstance(callbacks[0], TrainingProgressLogger)
+    gtw = Mock(spec=RedisSpectrumDataGateway)
+    train_model = TrainModel(gtw)
+    callbacks, settings = train_model._create_spec2vec_settings(
+        epochs=epochs, window=10
+    )
+    assert isinstance(callbacks[0], CustomTrainingProgressLogger)
     assert settings["iter"] == epochs
 
 
@@ -37,7 +43,9 @@ def test_spec2vec_settings():
     reason="It can only be run if the Redis is up",
 )
 def test_word2vec_training_with_iterator(documents_stored):
-    callbacks, settings = TrainModel._create_spec2vec_settings(epochs=2, window=10)
+    gtw = Mock(spec=RedisSpectrumDataGateway)
+    train_model = TrainModel(gtw)
+    callbacks, settings = train_model._create_spec2vec_settings(epochs=2, window=10)
     dgw = RedisSpectrumDataGateway()
     documents = dgw.read_documents_iter()
 
