6f59afac976c48239eb65b93a5aa0d3a4ade61b4
Tamara Lipowski <tamara@datarevenue.com>
2021-06-17 12:47:28 +0200

    Update predictor and test predictor to use ms2deep model and score calculation.
    --- don't change below this line ---
    MLOPS-288 #time 1h 31m

--- omigami/ms2deep/predictor.py
+++ omigami/ms2deep/predictor.py
@@ -1,8 +1,9 @@
 from logging import getLogger
 from typing import Union, List, Dict, Any, Tuple
 
-from gensim.models import Word2Vec
+from matchms.Spectrum import Spectrum
 from mlflow.pyfunc import PythonModel
+from ms2deepscore import MS2DeepScore
 
 
 log = getLogger(__name__)
@@ -12,35 +13,31 @@ SpectrumMatches = Dict[str, Dict[str, Any]]
 class Predictor(PythonModel):
     def __init__(
         self,
-        model: Word2Vec,
-        n_decimals: int,
-        intensity_weighting_power: Union[float, int],
-        allowed_missing_percentage: Union[float, int],
+        model: MS2DeepScore,
         run_id: str = None,
     ):
         self.model = model
-        self.n_decimals = n_decimals
-        self.intensity_weighting_power = intensity_weighting_power
-        self.allowed_missing_percentage = allowed_missing_percentage
         self.run_id = run_id
 
     def predict(
         self,
         context,
         data_input_and_parameters: Dict[str, Union[Dict, List]],
-        mz_range: int = 1,
     ) -> float:
-        """Match spectra from a json payload input with spectra having the highest similarity scores
-        in the GNPS spectra library.
-        Return a list matches of IDs and scores for each input spectrum.
-        """
         log.info("Creating a prediction.")
         data_input, parameters = self._parse_input(data_input_and_parameters)
-        log.info("Pre-processing data.")
-        pre_processed_data = self._pre_process_data(data_input)
+
+        spectrum_a = Spectrum(**data_input[0])
+        spectrum_b = Spectrum(**data_input[1])
+
+        score = self.score(spectrum_a, spectrum_b)
 
         log.info("Finishing prediction.")
-        return 0
+        return score
+
+    def score(self, spectrum_a: Spectrum, spectrum_b: Spectrum) -> float:
+        score = self.model.pair(spectrum_a, spectrum_b)
+        return score
 
     def set_run_id(self, run_id: str):
         self.run_id = run_id
@@ -52,16 +49,7 @@ class Predictor(PythonModel):
         if not isinstance(data_input_and_parameters, dict):
             data_input_and_parameters = data_input_and_parameters.tolist()
 
-        parameters = (
-            data_input_and_parameters.get("parameters")
-            if data_input_and_parameters.get("parameters")
-            else {}
-        )
+        parameters = data_input_and_parameters.get("parameters", {})
         data_input = data_input_and_parameters.get("data")
+
         return data_input, parameters
-
-    @staticmethod
-    def _pre_process_data(self, data_input: Dict[str, str]) -> List:
-        embeddings = data_input
-        return embeddings
-
--- omigami/test/ms2deep/test_predictor.py
+++ omigami/test/ms2deep/test_predictor.py
@@ -1,10 +1,12 @@
 import os
-import pickle
 from pathlib import Path
 
 import pytest
+from matchms.importing import load_from_mgf
 
-from omigami.predictor import Predictor
+from ms2deepscore import MS2DeepScore
+from omigami.ms2deep.predictor import Predictor
+from ms2deepscore.models import load_model
 from omigami.test.conftest import ASSETS_DIR
 
 
@@ -12,92 +14,57 @@ os.chdir(Path(__file__).parents[3])
 
 
 @pytest.fixture
-def small_payload():
-    small_payload = {
-        "parameters": {"n_best_spectra": 2},
-        "data": [
-            {
-                "peaks_json": "[[80.060677, 157.0], [337.508301, 230.0]]",
-            },
-            {
-                "peaks_json": "[[81.060677, 157.0], [327.508301, 230.0]]",
-            }
-        ],
-    }
-    return small_payload
+def spectrums():
+    spectrums = list(load_from_mgf(str(ASSETS_DIR / "pesticides_processed.mgf")))
+    return spectrums
 
 
 @pytest.fixture
-def big_payload():
-    big_payload = {
-        "parameters": {"n_best_spectra": 2},
+def payload(spectrums):
+    spectrum_a = spectrums[0]
+    spectrum_b = spectrums[1]
+    payload = {
+        "parameters": {},
         "data": [
             {
-                "peaks_json": "[[80.060677, 157.0], [81.072548, 295.0], [83.088249, 185.0], [91.057564, 601.0], "
-                "[95.085892, 634.0], [105.07077, 590.0], [107.09063, 228.0], [109.107414, 238.0], "
-                "[115.056114, 482.0], [117.07032, 715.0], [120.779747, 313.0], [121.067802, 257.0], "
-                "[121.102318, 430.0], [128.064728, 119.0], [129.074646, 241.0], [130.083542, 291.0], "
-                "[259.16922, 175.0], [273.219421, 190.0], [301.217468, 592.0], [1337.508301, 230.0]]",
+                "intensities": spectrum_a.peaks.intensities,
+                "mz": spectrum_a.peaks.mz,
             },
             {
-                "peaks_json": "[[80.060677, 157.0], [81.072548, 295.0], [83.088249, 185.0], [91.057564, 601.0], "
-                "[95.085892, 634.0], [105.07077, 590.0], [107.09063, 228.0], [109.107414, 238.0], "
-                "[115.056114, 482.0], [117.07032, 715.0], [120.779747, 313.0], [121.067802, 257.0], "
-                "[121.102318, 430.0], [128.064728, 119.0], [129.074646, 241.0], [130.083542, 291.0], "
-                "[259.16922, 175.0], [273.219421, 190.0], [301.217468, 592.0], [1337.508301, 230.0]]",
+                "intensities": spectrum_b.peaks.intensities,
+                "mz": spectrum_b.peaks.mz,
             },
         ],
     }
-    return big_payload
+    return payload
 
 
 @pytest.fixture()
-def model(word2vec_model):
-    return Predictor(
-        word2vec_model,
-        n_decimals=1,
-        intensity_weighting_power=0.5,
-        allowed_missing_percentage=25,
-        run_id="1",
-    )
-
-
-@pytest.fixture()
-def predict_parameters():
-    return {"n_best_spectra": 5}
-
-
-def test_pre_process_data(word2vec_model, model, small_payload):
-    data = small_payload["data"].items()
-    pre_processed_data = model._pre_process_data(data)
-    assert pre_processed_data
+def model():
+    path = str(ASSETS_DIR / "ms2deepscore_model.hdf5")
+    model = load_model(path)
+    model = MS2DeepScore(model)
+    return Predictor(model=model)
 
 
 @pytest.mark.skipif(
-    os.getenv("SKIP_REDIS_TEST", True),
-    reason="It can only be run if the Redis is up",
+    not os.path.exists(str(ASSETS_DIR / "ms2deepscore_model.hdf5")),
+    reason="ms2deepscore_model.hdf5 is git ignored",
 )
-@pytest.mark.skipif(
-    not os.path.exists(str(ASSETS_DIR / "test_model.pkl")),
-    reason="test_model.pkl is git ignored",
-)
-def test_local_predictions(small_payload, big_payload, redis_full_setup):
-    path = str(ASSETS_DIR / "test_model.pkl")
+def test_predictions(model, payload):
+    model.run_id = "1"
 
-    with open(path, "rb") as input_file:
-        local_model = pickle.load(input_file)
-
-    local_model.run_id = "1"
-
-    score = local_model.predict(
-        data_input_and_parameters=big_payload, mz_range=10, context=""
+    score = model.predict(
+        data_input_and_parameters=payload,
+        context="",
     )
 
     assert type(score) == float
 
 
-def test_parse_input(small_payload, model):
-    data_input, parameters = model._parse_input(small_payload)
+def test_parse_input(payload, model):
+    data_input, parameters = model._parse_input(payload)
 
-    assert "peaks_json" in data_input[0]
-
+    assert len(data_input) == 2
+    assert "intensities" in data_input[0]
+    assert "mz" in data_input[0]
