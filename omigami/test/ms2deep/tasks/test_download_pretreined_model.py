95111732ce7360d4f08424a9168cf27b9630788f
Pedro Martins <pedro@datarevenue.com>
2021-06-18 16:11:26 -0300

    Create Prefect task to download pretrained model
    --- don't change below this line ---
    MLOPS-287 #time 1h 3m

---
+++ omigami/test/ms2deep/tasks/test_download_pretreined_model.py
@@ -0,0 +1,54 @@
+from unittest.mock import MagicMock
+
+from drfs import DRPath
+from drfs.filesystems import get_fs
+from prefect import Flow
+
+from omigami.data_gateway import InputDataGateway
+from omigami.gateways.input_data_gateway import FSInputDataGateway
+from omigami.ms2deep.tasks.download_pre_trained_model import (
+    DownloadPreTrainedModelParameters,
+    DownloadPreTrainedModel,
+)
+from omigami.test.conftest import ASSETS_DIR
+
+
+def test_download_pre_trained_model(tmpdir, mock_default_config):
+
+    download_parameters = DownloadPreTrainedModelParameters(
+        model_uri="fake_uri", output_dir=tmpdir
+    )
+    input_dgw = MagicMock(spec=InputDataGateway)
+    input_dgw.download_ms2deep_model.return_value = download_parameters.output_path
+
+    with Flow("ms2deep-test-flow") as flow:
+        download = DownloadPreTrainedModel(input_dgw, download_parameters)()
+
+    res = flow.run()
+
+    assert res.is_successful()
+    assert res.result[download].result == download_parameters.output_path
+
+    input_dgw.download_ms2deep_model.assert_called_once_with(
+        download_parameters.model_uri, download_parameters.output_path
+    )
+
+
+def test_download_pretrained_model_existing(mock_default_config):
+
+    input_dgw = FSInputDataGateway()
+    input_dgw.download_ms2deep_model = lambda *args: None
+
+    download_parameters = DownloadPreTrainedModelParameters(
+        model_uri="fake_uri",
+        output_dir=ASSETS_DIR / "ms2deep" / "pretrained",
+        file_name="fake_pretrained_model.hdf5",
+    )
+
+    with Flow("ms2deep-test-flow") as flow:
+        download = DownloadPreTrainedModel(input_dgw, download_parameters)()
+
+    res = flow.run()
+
+    assert res.is_successful()
+    assert res.result[download].is_cached()
