92de1d403d96e8da5753188d30603ece0bfdda37
Pedro Martins <pedro@datarevenue.com>
2021-06-21 18:16:15 -0300

    Add test to for spec2vec register_model.py
    --- don't change below this line ---
    MLOPS-287
    
    #time 17m

---
+++ omigami/test/spec2vec/tasks/test_register_model.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+
+from omigami.spec2vec.predictor import Predictor
+from omigami.spec2vec.tasks.register_model import ModelRegister
+
+os.chdir(Path(__file__).parents[4])
+
+
+def test_register_model(word2vec_model, tmpdir):
+    path = f"{tmpdir}/mlflow/"
+    model_register = ModelRegister(path)
+    run_id = model_register.register_model(
+        Predictor(
+            word2vec_model,
+            n_decimals=2,
+            intensity_weighting_power=0.5,
+            allowed_missing_percentage=5.0,
+        ),
+        "experiment",
+        path,
+    )
+    assert run_id
+    assert os.path.exists(f"{path}/model/python_model.pkl")
+    assert os.path.exists(f"{path}/model/conda.yaml")
+    assert "spec2vec" in os.listdir(f"{path}/model/code")
