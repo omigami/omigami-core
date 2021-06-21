4eeb6d60396cde4862e3f3f795615469a7fa0a5d
Pedro Martins <pedro@datarevenue.com>
2021-06-21 17:44:13 -0300

    Model Register abstract class
    add test-output.xml to .gitignore
    
    --- don't change below this line ---
    MLOPS-287
    
    #time 1m

---
+++ omigami/model_register.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+import mlflow
+from mlflow.exceptions import MlflowException
+from mlflow.pyfunc import PythonModel
+
+
+class MLFlowModelRegister(ABC):
+    def __init__(self, server_uri: str):
+        mlflow.set_tracking_uri(server_uri)
+
+    @staticmethod
+    def _get_or_create_experiment_id(experiment_name: str, path: str) -> str:
+        experiment = mlflow.get_experiment_by_name(experiment_name)
+        if experiment:
+            return experiment.experiment_id
+        else:
+            return mlflow.create_experiment(experiment_name, artifact_location=path)
+
+    @staticmethod
+    def log_model(
+        model: PythonModel,
+        experiment_name: str,
+        path: str,
+        code_path: List[str],
+        conda_env_path: str = None,
+    ):
+        try:
+            mlflow.pyfunc.log_model(
+                "model",
+                python_model=model,
+                registered_model_name=experiment_name,
+                conda_env=conda_env_path,
+                code_path=code_path,
+            )
+        # This is need to run the flow locally. mlflow.pyfunc.log_model is not
+        # supported without a database.
+        except MlflowException:
+            mlflow.pyfunc.save_model(
+                f"{path}/model",
+                python_model=model,
+                conda_env=conda_env_path,
+                code_path=code_path,
+            )
+
+    @abstractmethod
+    def register_model(
+        self,
+        model: PythonModel,
+        experiment_name: str,
+        path: str,
+        conda_env_path: str = None,
+    ):
+        # TODO: Proper add docstring
+        """
+        Parameters
+        ----------
+        model
+        experiment_name
+        path
+        conda_env_path
+
+        Returns
+        -------
+
+        """
+        pass
