1668e0109b7b8a2b2eb9edb66d1fbf7cf1c796fc
Diana Carvalho <diana@datarevenue.com>
2021-06-23 15:10:02 -0500

    Move flow_config outside of spec2vec
    --- don't change below this line ---
    MLOPS-287 #time 14m

--- omigami/test/spec2vec/flows/test_training_flow.py
+++ omigami/test/spec2vec/flows/test_training_flow.py
@@ -7,7 +7,7 @@ from drfs.filesystems import get_fs
 
 from omigami.spec2vec.config import SOURCE_URI_PARTIAL_GNPS
 from omigami.data_gateway import SpectrumDataGateway
-from omigami.spec2vec.flows.config import (
+from omigami.flow_config import (
     make_flow_config,
     PrefectStorageMethods,
     PrefectExecutorMethods,
