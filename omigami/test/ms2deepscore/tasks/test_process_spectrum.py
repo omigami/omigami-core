7185b31ac5e320e89e4df86904788aa8a4d06626
Tamara Lipowski <tamara@datarevenue.com>
2021-06-29 18:22:52 +0200

    Add test for ms2deepscore filters.
    --- don't change below this line ---
    MLOPS-318 #time 23m

--- omigami/test/ms2deepscore/tasks/test_process_spectrum.py
+++ omigami/test/ms2deepscore/tasks/test_process_spectrum.py
@@ -1,11 +1,21 @@
+import pytest
 from matchms import Spectrum
+from matchms.importing.load_from_json import as_spectrum
 
 from omigami.ms2deepscore.tasks.spectrum_processor import SpectrumProcessor
 
 
-def test_process_spectra(loaded_data):
-    spectrum_processor = SpectrumProcessor()
+@pytest.fixture
+def spectrum_processor():
+    return SpectrumProcessor()
 
+
+@pytest.fixture
+def spectrum(loaded_data):
+    return as_spectrum(loaded_data[0])
+
+
+def test_process_spectra(loaded_data, spectrum_processor):
     cleaned_data = spectrum_processor.process_spectra(loaded_data)
 
     assert isinstance(cleaned_data[0], Spectrum)
@@ -15,3 +25,21 @@ def test_process_spectra(loaded_data):
     assert isinstance(cleaned_data[0].get("charge"), int)
     assert cleaned_data[0].get("parent_mass")
     assert cleaned_data[0].get("spectrum_id")
+
+
+def test_apply_ms2deepscore_filters(spectrum, spectrum_processor):
+    mz_from = 10.0
+    mz_to = 1000.0
+
+    filtered_spectrum = spectrum_processor._apply_ms2deepscore_filters(spectrum)
+
+    assert filtered_spectrum is not None
+    assert all([mz_from <= mz <= mz_to for mz in filtered_spectrum.peaks.mz])
+
+    spectrum_with_not_enough_peaks = Spectrum(
+        mz=spectrum.peaks.mz[:4], intensities=spectrum.peaks.intensities[:4]
+    )
+    filtered_spectrum = spectrum_processor._apply_ms2deepscore_filters(
+        spectrum_with_not_enough_peaks
+    )
+    assert filtered_spectrum is None
