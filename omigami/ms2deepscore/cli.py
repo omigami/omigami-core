ec2927acafc5ab1b1a34bc598303cc22ba598738
Pedro Martins <pedro@datarevenue.com>
2021-07-01 16:25:30 -0300

    Change cli.py and deployment.py of spec2vec
    --- don't change below this line ---
    MLOPS-287 #time 1m

--- omigami/ms2deepscore/cli.py
+++ omigami/ms2deepscore/cli.py
@@ -1,32 +1,30 @@
 import click
-
 from omigami.config import (
-    SOURCE_URI_PARTIAL_GNPS,
     API_SERVER_URLS,
     PROJECT_NAME,
-    S3_BUCKETS,
-    MODEL_DIRECTORIES,
     MLFLOW_SERVER,
 )
-from omigami.ms2deep.deployment import deploy_training_flow
-from omigami.utils import add_options
+from omigami.ms2deepscore.deployment import deploy_minimal_flow
+from omigami.utils import add_click_options
 
-configuration_options = [
-    click.option("--project-name", "-p", default=PROJECT_NAME),
-    click.option("--source-uri", default=SOURCE_URI_PARTIAL_GNPS),
-    click.option("--output-dir", default=S3_BUCKETS),
-    click.option("--model-output-dir", default=MODEL_DIRECTORIES),
-    click.option("--mlflow-server", default=MLFLOW_SERVER),
-]
 
 auth_options = [
     click.option(
         "--api-server", default=API_SERVER_URLS["dev"], help="URL to the prefect API"
     ),
     click.option("--auth", default=False, help="Enable authentication"),
-    click.option("--auth_url", default=None, help="Kratos Public URI"),
+    click.option("--auth-url", default=None, help="Kratos Public URI"),
     click.option("--username", default=None, help="Login username"),
     click.option("--password", default=None, help="Login password"),
+]
+
+configuration_options = [
+    click.option("--project-name", "-p", default=PROJECT_NAME),
+    click.option("--mlflow-server", default=MLFLOW_SERVER),
+    click.option("--flow-name", default="ms2deepscore-minimal-flow"),
+    click.option("--environment", "--env", default="dev"),
+    click.option("--deploy-model", is_flag=True),
+    click.option("--overwrite", is_flag=True, help="Overwrite existing model"),
 ]
 
 
@@ -37,10 +35,10 @@ def cli():
 
 @cli.command(name="register-training-flow")
 @click.option("--image", "-i", type=str, required=True)
-@add_options(auth_options)
-@add_options(configuration_options)
+@add_click_options(auth_options)
+@add_click_options(configuration_options)
 def deploy_training_flow_cli(*args, **kwargs):
-    deploy_training_flow(*args, **kwargs)
+    deploy_minimal_flow(*args, **kwargs)
 
 
 if __name__ == "__main__":
