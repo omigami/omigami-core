5644ad4acbbab59572cae96a1f0259337267b0a4
Diana Carvalho <diana@datarevenue.com>
2021-07-07 09:24:54 +0100

    Uncomment skip on test_deploy_minimal_flow
    --- don't change below this line ---
    MLOPS-323 #time 2m

--- omigami/test/ms2deepscore/test_ms2deepscore_deployment.py
+++ omigami/test/ms2deepscore/test_ms2deepscore_deployment.py
@@ -5,9 +5,9 @@ from omigami.config import config, MLFLOW_SERVER
 from omigami.ms2deepscore.deployment import deploy_minimal_flow
 
 
-# @pytest.mark.skip(
-#     reason="This test uses internet connection and deploys a test flow to prefect."
-# )
+@pytest.mark.skip(
+    reason="This test uses internet connection and deploys a test flow to prefect."
+)
 def test_deploy_minimal_flow():
     """
     BE CAREFUL -> DO NOT set `deploy_model=True` and `env="prod"` unless you know exactly
