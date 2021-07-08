93247fa26c85b856e7e09fd2681a946b781f048e
Tamara Lipowski <tamara@datarevenue.com>
2021-07-08 11:09:02 +0200

    Call fit_transform instead of transform from spectrum binner to avoid problem of missing vocabulary.
    --- don't change below this line ---
    MLOPS-334 #time 13m

--- omigami/ms2deepscore/helper_classes/spectrum_binner.py
+++ omigami/ms2deepscore/helper_classes/spectrum_binner.py
@@ -8,7 +8,7 @@ class MS2DeepScoreSpectrumBinner:
     def bin_spectra(self, spectra: List[Spectrum]) -> List[BinnedSpectrum]:
         spectrum_binner = SpectrumBinner(number_of_bins=10000)
         spectra_ids = [spectrum.metadata["spectrum_id"] for spectrum in spectra]
-        binned_spectra = spectrum_binner.transform(spectra)
+        binned_spectra = spectrum_binner.fit_transform(spectra)
         binned_spectra = [
             spectrum.set("spectrum_id", spectra_ids[i])
             for i, spectrum in enumerate(binned_spectra)
