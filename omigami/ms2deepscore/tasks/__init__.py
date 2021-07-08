677826cd71f99146894374b697d30f9134b36324
Rouven Reuter <rouven@datarevenue.com>
2021-07-08 21:08:34 +0200

    [omigami/ms2deepscore/tasks/__init__.py] Removed download task import

--- omigami/ms2deepscore/tasks/__init__.py
+++ omigami/ms2deepscore/tasks/__init__.py
@@ -1,3 +1,2 @@
 from omigami.ms2deepscore.tasks.deploy_model import DeployModel, DeployModelParameters
 from omigami.ms2deepscore.tasks.register_model import RegisterModel
-from omigami.ms2deepscore.tasks.download_data import DownloadData, DownloadParameters
