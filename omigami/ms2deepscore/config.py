37ed0b4bc67142db8ff1639b27ac57bb9c6d063b
Rouven <rouven@datarevenue.com>
2021-07-08 14:00:59 +0200

    [omigami/ms2deepscore/config.py] Loads config for download data task [omigami/ms2deepscore/config_default.yaml] Added dataset uri [omigami/test/ms2deepscore/tasks/test_download_data.py] Implimented test by copying from spec2vec

--- omigami/ms2deepscore/config.py
+++ omigami/ms2deepscore/config.py
@@ -7,5 +7,5 @@ class Configuration(confuse.Configuration):
 
 
 config = Configuration("omigami/ms2deepscore", __name__)
-
+SOURCE_URI_PARTIAL_GNPS = config["gnps_uri"]["partial"].get(str)
 MODEL_DIRECTORIES = config["storage"]["model_folder"].get(dict)
