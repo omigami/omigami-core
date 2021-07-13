01fe7ba4a6bcd51807a10e7a79b9e22b78ab93da
Diana Carvalho <diana@datarevenue.com>
2021-07-13 10:13:09 +0100

    Rename "not_exist" to "missing"
    --- don't change below this line ---
    MLOPS-338 #time 8m

--- omigami/ms2deepscore/gateways/redis_spectrum_gateway.py
+++ omigami/ms2deepscore/gateways/redis_spectrum_gateway.py
@@ -15,12 +15,12 @@ from omigami.ms2deepscore.config import BINNED_SPECTRUM_HASHES
 class MS2DeepScoreRedisSpectrumDataGateway(RedisSpectrumDataGateway):
     """Data gateway for Redis storage."""
 
-    def list_binned_spectra_not_exist(self, spectrum_ids: List[str]) -> List[str]:
+    def list_missing_binned_spectra(self, spectrum_ids: List[str]) -> List[str]:
         """Check whether document exist on Redis.
         Return a list of IDs that do not exist.
         """
         self._init_client()
-        return self._list_spectrum_ids_not_exist(BINNED_SPECTRUM_HASHES, spectrum_ids)
+        return self._list_missing_spectrum_ids(BINNED_SPECTRUM_HASHES, spectrum_ids)
 
     def write_binned_spectra(
         self, binned_spectra: List[BinnedSpectrum], logger: Logger = None
