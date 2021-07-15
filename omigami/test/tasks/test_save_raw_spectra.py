00a44e572a377c3ca5736647a6ca67edc96fe859
Rouven Reuter <rouven@datarevenue.com>
2021-07-15 09:08:11 +0200

    Repsoitioned tests and added new assert to test adding new ids

--- omigami/test/tasks/test_save_raw_spectra.py
+++ omigami/test/tasks/test_save_raw_spectra.py
@@ -21,6 +21,24 @@ def create_parameters():
     return parameters
 
 
+def test_save_raw_spectra_skip(local_gnps_small_json):
+    """Test if skipping the task wörks"""
+    # Setup Test
+    parameters = create_parameters()
+    parameters.skip_task = True
+    # Run Functions
+    with Flow("test-flow") as test_flow:
+        raw_spectra = SaveRawSpectra(save_parameters=parameters)(local_gnps_small_json)
+
+    res = test_flow.run()
+    data = res.result[raw_spectra].result
+
+    # Test Results
+    assert res.is_successful()
+    assert len(parameters.spectrum_dgw.list_spectrum_ids()) == 0
+    assert len(data) == 0
+
+
 def test_save_raw_spectra_empty_db(local_gnps_small_json):
     """Test if new spectra get added to an empty database"""
     # Setup Test
@@ -44,7 +62,12 @@ def test_save_raw_spectra_adding_new_spectra(local_gnps_small_json):
     # Setup Test
     parameters = create_parameters()
     spectra = parameters.input_dgw.load_spectrum(local_gnps_small_json)
+    parameters.spectrum_dgw.delete_spectra(
+        [spec_id["spectrum_id"] for spec_id in spectra]
+    )
     parameters.spectrum_dgw.write_raw_spectra(spectra[:30])
+
+    assert len(parameters.spectrum_dgw.list_spectrum_ids()) == 30
 
     # Run Functions
     with Flow("test-flow") as test_flow:
@@ -57,21 +80,3 @@ def test_save_raw_spectra_adding_new_spectra(local_gnps_small_json):
     assert res.is_successful()
     assert len(parameters.spectrum_dgw.list_spectrum_ids()) == 100
     assert len(data) == 100
-
-
-def test_save_raw_spectra_skip(local_gnps_small_json):
-    """Test if skipping the task wörks"""
-    # Setup Test
-    parameters = create_parameters()
-    parameters.skip_task = True
-    # Run Functions
-    with Flow("test-flow") as test_flow:
-        raw_spectra = SaveRawSpectra(save_parameters=parameters)(local_gnps_small_json)
-
-    res = test_flow.run()
-    data = res.result[raw_spectra].result
-
-    # Test Results
-    assert res.is_successful()
-    assert len(parameters.spectrum_dgw.list_spectrum_ids()) == 0
-    assert len(data) == 0
