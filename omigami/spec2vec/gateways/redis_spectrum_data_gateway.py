faa463818aeaf8291d5cd8be85b06919588758d0
Rouven Reuter <rouven@datarevenue.com>
2021-07-15 10:41:57 +0200

    Changed from using pipeline to using clinet object directrly

--- omigami/spec2vec/gateways/redis_spectrum_data_gateway.py
+++ omigami/spec2vec/gateways/redis_spectrum_data_gateway.py
@@ -107,12 +107,12 @@ class RedisSpectrumDataGateway(SpectrumDataGateway):
 
     def write_raw_spectra(self, spectra):
         self._init_client()
-        pipe = self.client.pipeline()
 
         for spectrum in spectra:
             # spectrum_info = spectrum.spectrum
-            pipe.hset(SPECTRUM_HASHES, spectrum["spectrum_id"], pickle.dumps(spectrum))
-        pipe.execute()
+            self.client.hset(
+                SPECTRUM_HASHES, spectrum["spectrum_id"], pickle.dumps(spectrum)
+            )
 
     def write_embeddings(
         self, embeddings: List[Embedding], run_id: str, logger: Logger = None
