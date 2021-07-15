f70e5c2f2a882b72a387ac35f24badee7088aa4c
Diana Carvalho <diana@datarevenue.com>
2021-07-15 10:20:35 +0100

    Fix broken test
    --- don't change below this line ---
    MLOPS-328 #time 0m

--- omigami/test/ms2deepscore/helper_classes/test_spectrum_binner.py
+++ omigami/test/ms2deepscore/helper_classes/test_spectrum_binner.py
@@ -1,28 +1,13 @@
-import os
-import pytest
 from ms2deepscore import BinnedSpectrum
-
-from omigami.ms2deepscore.helper_classes.spectrum_binner import SpectrumBinner
-from omigami.test.conftest import ASSETS_DIR
-
-pytestmark = pytest.mark.skipif(
-    not os.path.exists(
-        str(
-            ASSETS_DIR
-            / "ms2deepscore"
-            / "pretrained"
-            / "MS2DeepScore_allGNPSpositive_10k_500_500_200.hdf5"
-        )
-    ),
-    reason="MS2DeepScore_allGNPSpositive_10k_500_500_200.hdf5 is git ignored. Please "
-    "download it from https://zenodo.org/record/4699356#.YNyD-2ZKhcA",
+from omigami.ms2deepscore.helper_classes.spectrum_binner import (
+    MS2DeepScoreSpectrumBinner,
 )
 
 
 def test_bin_spectra(cleaned_data_ms2deep_score, ms2deepscore_real_model_path):
-    spectrum_binner = SpectrumBinner()
+    spectrum_binner = MS2DeepScoreSpectrumBinner()
     binned_spectra = spectrum_binner.bin_spectra(
-        cleaned_data_ms2deep_score, model_path=ms2deepscore_real_model_path
+        cleaned_data_ms2deep_score,
     )
 
     assert isinstance(binned_spectra[0], BinnedSpectrum)
