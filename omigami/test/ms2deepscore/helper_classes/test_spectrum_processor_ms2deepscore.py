df51ba905ea9f3de31dabde57cfa01adc0e0a2a2
Diana Carvalho <diana@datarevenue.com>
2021-07-16 17:26:32 +0100

    Fix tests by updating asset
    --- don't change below this line ---
    MLOPS-351 #time 12m

--- omigami/test/ms2deepscore/helper_classes/test_spectrum_processor_ms2deepscore.py
+++ omigami/test/ms2deepscore/helper_classes/test_spectrum_processor_ms2deepscore.py
@@ -30,10 +30,9 @@ def test_process_spectra(positive_spectra_data, spectrum_processor):
 
 
 def test_process_spectra_already_converted_type_spectrum(
-    loaded_data, spectrum_processor
+    basic_cleaned_data, spectrum_processor
 ):
-    converted_spectra_data = [as_spectrum(s) for s in loaded_data]
-    cleaned_data = spectrum_processor.process_spectra(converted_spectra_data)
+    cleaned_data = spectrum_processor.process_spectra(basic_cleaned_data)
 
     assert isinstance(cleaned_data[0], Spectrum)
 
@@ -64,8 +63,12 @@ def test_apply_ms2deepscore_filters_not_enough_peaks(spectrum, spectrum_processo
     assert filtered_spectrum is None
 
 
-def test_run_missing_smiles_inchi_against_pubchem(loaded_data, spectrum_processor):
-    cleaned_spectrum = spectrum_processor.process_spectra([loaded_data[2]], False)
+def test_run_missing_smiles_inchi_against_pubchem(
+    basic_cleaned_data, spectrum_processor
+):
+    cleaned_spectrum = spectrum_processor.process_spectra(
+        [basic_cleaned_data[2]], False
+    )
     spectrum_with_inchikey = spectrum_processor._get_missing_inchis(cleaned_spectrum[0])
 
     assert not cleaned_spectrum[0].metadata.get("inchikey")
