5d5fafdc29d5c60876c85cae26930868be37b37d
Diana Carvalho <diana@datarevenue.com>
2021-07-27 11:37:11 +0100

    Update image tag and comment on deployment test
    --- don't change below this line ---
    MLOPS-331 #time 12m

--- omigami/test/ms2deepscore/test_ms2deepscore_deployment.py
+++ omigami/test/ms2deepscore/test_ms2deepscore_deployment.py
@@ -47,8 +47,10 @@ def test_deploy_training_flow():
     login_config.pop("token")
 
     deployer = MS2DeepScoreDeployer(
-        image="drtools/prefect:omigami-SNAPSHOT.48aa979",
+        image="drtools/prefect:omigami-SNAPSHOT.b8908e8",
         dataset_name="10k",  # ms2deepscore can not be trained with the small dataset
+        # because the minimum batch size to train is 32 samples and the small dataset
+        # will lead to less samples than that.
         environment=env,
         project_name="ms2deepscore-dev",
         mlflow_server=MLFLOW_SERVER,
