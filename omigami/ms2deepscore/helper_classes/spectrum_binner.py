8f516126275d243c3cb00b156dd09b8ffaee9154
Diana Carvalho <diana@datarevenue.com>
2021-07-30 08:35:43 +0100

    Take out binner to improve error message
    --- don't change below this line ---
    MLOPS-369 #time 25m

--- omigami/ms2deepscore/helper_classes/spectrum_binner.py
+++ omigami/ms2deepscore/helper_classes/spectrum_binner.py
@@ -2,6 +2,12 @@ from typing import List
 
 from matchms import Spectrum
 from ms2deepscore import BinnedSpectrum, SpectrumBinner
+from ms2deepscore.spectrum_binning_fixed import (
+    unique_peaks_fixed,
+    create_peak_list_fixed,
+)
+from ms2deepscore.utils import create_peak_dict
+from tqdm import tqdm
 
 
 class MS2DeepScoreSpectrumBinner:
@@ -9,7 +15,43 @@ class MS2DeepScoreSpectrumBinner:
         self.spectrum_binner = SpectrumBinner(number_of_bins=n_bins)
 
     def bin_spectra(self, spectra: List[Spectrum]) -> List[BinnedSpectrum]:
-        binned_spectra = self.spectrum_binner.fit_transform(spectra)
+        # binned_spectra = self.spectrum_binner.fit_transform(spectra)
+
+        print("Collect spectrum peaks...")
+        peak_to_position, known_bins = unique_peaks_fixed(
+            spectra,
+            self.spectrum_binner.d_bins,
+            self.spectrum_binner.mz_max,
+            self.spectrum_binner.mz_min,
+        )
+        print(f"Calculated embedding dimension: {len(known_bins)}.")
+        self.spectrum_binner.peak_to_position = peak_to_position
+        self.spectrum_binner.known_bins = known_bins
+
+        print("Convert spectrums to binned spectrums...")
+
+        peak_lists, missing_fractions = create_peak_list_fixed(
+            spectra,
+            self.spectrum_binner.peak_to_position,
+            self.spectrum_binner.d_bins,
+            mz_max=self.spectrum_binner.mz_max,
+            mz_min=self.spectrum_binner.mz_min,
+            peak_scaling=self.spectrum_binner.peak_scaling,
+            progress_bar=True,
+        )
+        binned_spectra = []
+        for i, peak_list in enumerate(
+            tqdm(peak_lists, desc="Create BinnedSpectrum instances", disable=(not True))
+        ):
+            assert (
+                100 * missing_fractions[i]
+                <= self.spectrum_binner.allowed_missing_percentage
+            ), f"{100*missing_fractions[i]:.2f} of weighted spectrum is unknown to the model. GUILY={spectra[i].get('spectrum_id')}"
+            spectrum = BinnedSpectrum(
+                binned_peaks=create_peak_dict(peak_list),
+                metadata={"inchikey": spectra[i].get("inchikey")},
+            )
+            binned_spectra.append(spectrum)
 
         for binned_spectrum, spectrum in zip(binned_spectra, spectra):
             binned_spectrum.set("spectrum_id", spectrum.get("spectrum_id"))
