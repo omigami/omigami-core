754cfaca8d05703da8921316a84e71706c70833d
bernardolk <bernardo.knackfuss@gmail.com>
2021-10-08 15:12:23 -0300

    making sure platform is correct when making image, even on M1's

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ TAG="drtools/prefect:omigami$(snap)"
 
 echo "$TAG"
 
-docker build -t $TAG -f default.Dockerfile .
+docker build -t $TAG -f default.Dockerfile . --platform linux/amd64
 docker push $TAG
 
 echo "Successfully pushed $TAG"
