a9ce107230936a0b90bb1b5810cc10f6324be1e8
cereniyim <iyimceren@gmail.com>
2021-11-22 11:45:05 +0300

    delete word2vec_model.pickle and train a model in word2vec_model fixture instead
    update unit tests accordingly
    
    --- don't change below this line ---
    MLOPS-402 #time 1h 12m

--- omigami/test/conftest.py
+++ omigami/test/conftest.py
@@ -10,6 +10,7 @@ import s3fs
 from drfs.filesystems import get_fs
 from moto import mock_s3
 from pytest_redis import factories
+from spec2vec.model_building import train_new_word2vec_model
 
 import omigami
 import omigami.config
@@ -88,14 +89,6 @@ def cleaned_data():
     with open(path, "rb") as handle:
         cleaned_data = pickle.load(handle)
     return cleaned_data
-
-
-@pytest.fixture(scope="module")
-def word2vec_model():
-    path = str(ASSETS_DIR / "word2vec_model.pickle")
-    with open(path, "rb") as handle:
-        model = pickle.load(handle)
-    return model
 
 
 @pytest.fixture(scope="module")
@@ -218,11 +211,43 @@ def redis_full_setup(
 
 @pytest.fixture(scope="module")
 def documents_data():
+    """
+    Fixture containing `SpectrumDocument` objects whose `n_decimals` is 1.
+
+    Returns
+    -------
+    List of `SpectrumDocument`s
+
+    """
     path = str(ASSETS_DIR / "SMALL_GNPS_as_documents.pickle")
     with open(path, "rb") as handle:
         documents_data = pickle.load(handle)
 
     return documents_data
+
+
+@pytest.fixture(scope="module")
+def word2vec_model(documents_data):
+    """
+    This fixture will only work with `SpectrumDocument`s where `n_decimals` equal to 1,
+    because it is trained on this data.
+
+    Parameters
+    ----------
+    documents_data: List of `SpectrumDocument` with `n_decimals` set to 1.
+
+    Returns
+    -------
+    Word2Vec model
+
+    """
+
+    model = train_new_word2vec_model(
+        documents_data,
+        size=300,
+        iterations=3
+    )
+    return model
 
 
 @pytest.fixture()
