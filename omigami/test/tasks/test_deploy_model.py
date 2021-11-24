f253c81c024273081027266e072fba05b370b5a4
czanella <carlos@datarevenue.com>
2021-11-24 10:04:47 +0000

    Remove test that used to deploy to k8s. Move deploymodel test to shared
    #time 10m

--- omigami/test/tasks/test_deploy_model.py
+++ omigami/test/tasks/test_deploy_model.py
@@ -5,24 +5,6 @@ from prefect.storage import S3
 from omigami.tasks.deploy_model import DeployModelParameters, DeployModel
 
 
-@pytest.mark.skip(
-    reason="This test is actually deploying to seldon we should change asap"
-)
-def test_deploy_model_task():
-    params = DeployModelParameters(
-        redis_db="2", ion_mode="neutral", overwrite_model=True, environment="dev"
-    )
-    with Flow("test-flow") as test_flow:
-        deploy_task = DeployModel(params)(registered_model={"model_uri": "uri"})
-
-        res = test_flow.run()
-        data = res.result[deploy_task].result
-
-        assert res.is_successful()
-
-    assert True
-
-
 @pytest.mark.skip(reason="This test deploys a seldon model using a model URI.")
 def test_deploy_seldon_model(mock_default_config):
     FLOW_CONFIG = {
