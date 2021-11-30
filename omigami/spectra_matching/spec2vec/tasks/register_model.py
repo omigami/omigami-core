834cdd084eeef3dcd95c634f999c7739b7f0ee30
czanella <carlos@datarevenue.com>
2021-11-30 15:29:05 +0000

    Unify model registry dgw between s2v and ms2ds. Rewrite tests.
    #time 1h 3m

--- omigami/spectra_matching/spec2vec/tasks/register_model.py
+++ omigami/spectra_matching/spec2vec/tasks/register_model.py
@@ -6,9 +6,9 @@ from gensim.models import Word2Vec
 from pandas import Timestamp
 from prefect import Task
 
-from omigami.config import IonModes, CONDA_ENV_PATH, CODE_PATH, MLFLOW_SERVER
-from omigami.spectra_matching.model_register import MLFlowModelRegister
+from omigami.config import IonModes, CONDA_ENV_PATH, MLFLOW_SERVER
 from omigami.spectra_matching.spec2vec.predictor import Spec2VecPredictor
+from omigami.spectra_matching.storage.model_registry import MLFlowDataGateway
 from omigami.utils import merge_prefect_task_configs
 
 
@@ -60,62 +60,37 @@ class RegisterModel(Task):
 
         """
         self.logger.info(f"Registering model to {MLFLOW_SERVER} on URI: {self._path}.")
-        run_name = f"spec2vec-{Timestamp.now():%Y%m%dT%H%M}"
-        model_register = ModelRegister(MLFLOW_SERVER)
+        run_name = f"spec2vec-{self._ion_mode}-{Timestamp.now():%Y%m%dT%H%M}"
+        model_register = MLFlowDataGateway(MLFLOW_SERVER)
+        spec2vec_model = Spec2VecPredictor(
+            model,
+            self._ion_mode,
+            self._n_decimals,
+            self._intensity_weighting_power,
+            self._allowed_missing_percentage,
+        )
+
+        params = {
+            "n_decimals_for_documents": spec2vec_model.n_decimals,
+            "intensity_weighting_power": spec2vec_model.intensity_weighting_power,
+            "allowed_missing_percentage": spec2vec_model.allowed_missing_percentage,
+            "iter": spec2vec_model.model.epochs,
+            "window": spec2vec_model.model.window,
+        }
+        metrics = {"alpha": spec2vec_model.model.alpha}
+
         run_id = model_register.register_model(
-            Spec2VecPredictor(
-                model,
-                self._ion_mode,
-                self._n_decimals,
-                self._intensity_weighting_power,
-                self._allowed_missing_percentage,
-            ),
+            spec2vec_model,
             experiment_name=self._experiment_name,
             output_path=self._path,
             conda_env_path=CONDA_ENV_PATH,
             run_name=run_name,
             model_name=self._model_name,
+            params=params,
+            metrics=metrics,
         )
         run = mlflow.get_run(run_id)
         self.logger.info(f"{run.info}")
 
         model_uri = f"{run.info.artifact_uri}/model/"
         return {"model_uri": model_uri, "run_id": run_id}
-
-
-class ModelRegister(MLFlowModelRegister):
-    # TODO: refactor this class. The concrete implementation should inherit the
-    # TODO: abstraction, and here it is the other way around???
-    def register_model(
-        self,
-        model: Spec2VecPredictor,
-        experiment_name: str,
-        output_path: str = None,
-        conda_env_path: str = None,
-        run_name: str = "spec2vec",
-        model_name: Optional[str] = None,
-    ) -> str:
-        experiment_id = self._get_or_create_experiment_id(experiment_name, output_path)
-        with mlflow.start_run(
-            run_name=run_name, experiment_id=experiment_id, nested=True
-        ) as run:
-            params = {
-                "n_decimals_for_documents": model.n_decimals,
-                "intensity_weighting_power": model.intensity_weighting_power,
-                "allowed_missing_percentage": model.allowed_missing_percentage,
-                "iter": model.model.epochs,
-                "window": model.model.window,
-            }
-            mlflow.log_params(params)
-            run_id = run.info.run_id
-            model.set_run_id(run_id)
-            mlflow.pyfunc.log_model(
-                "model",
-                python_model=model,
-                registered_model_name=model_name,
-                code_path=[CODE_PATH],
-                conda_env=conda_env_path,
-            )
-
-            mlflow.log_metric("alpha", model.model.alpha)
-            return run_id
