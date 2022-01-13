48f7637db59ad191330ac57cceca1a2d84660f9b
cereniyim <iyimceren@gmail.com>
2022-01-13 15:20:00 +0300

    implement ListCleanedSpectraPaths task
    --- don't change below this line ---
    OMI-512 #time 1h 23m

---
+++ test/spectra_matching/tasks/test_list_cleaned_spectra_paths.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from omigami.spectra_matching.storage import FSDataGateway
+from spectra_matching.tasks.list_cleaned_spectra_paths import ListCleanedSpectraPaths
+
+
+def test_list_cleaned_spectra_path(tmpdir):
+    expected_paths = []
+    for i in range(10):
+        path = Path(str(tmpdir / f"clenaed_spectra-{i}"))
+        path.touch()
+        expected_paths.append(str(path))
+
+    fs_dgw = FSDataGateway()
+
+    task = ListCleanedSpectraPaths(tmpdir, fs_dgw)
+    cleaned_spectra_paths = task.run()
+
+    assert len(cleaned_spectra_paths) == 10
+    assert {str(p) for p in cleaned_spectra_paths} == set(expected_paths)
